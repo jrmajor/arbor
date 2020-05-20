6410a32f39663b27962db8113749b57c642e7704
maksiuP <maksiu@npng.pl>
2020-05-20 21:22:32 +0200

    update template to laravel 7.12

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -7,6 +7,7 @@ use Illuminate\Foundation\Http\Kernel as HttpKernel;
 class Kernel extends HttpKernel
 {
     protected $middleware = [
+        // \App\Http\Middleware\TrustHosts::class,
         \App\Http\Middleware\TrustProxies::class,
         \Fruitcake\Cors\HandleCors::class,
         \App\Http\Middleware\CheckForMaintenanceMode::class,
