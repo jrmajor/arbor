598e16aafd8086a6b0fcf474c0202ee275180f79
maksiuP <maksiu@npng.pl>
2020-05-23 00:52:56 +0200

    add sitemap generator

---
+++ app/Console/Commands/GenerateSitemap.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Person;
+use Illuminate\Console\Command;
+use Spatie\Sitemap\Sitemap;
+use Spatie\Sitemap\Tags\Url;
+
+class GenerateSitemap extends Command
+{
+    protected $signature = 'sitemap:generate';
+
+    protected $description = 'Generate sitemap.';
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function handle()
+    {
+        $sitemap = Sitemap::create()
+
+            ->add(Url::create('/')
+                ->setChangeFrequency(Url::CHANGE_FREQUENCY_WEEKLY))
+            ->add(Url::create('/people')
+                ->setChangeFrequency(Url::CHANGE_FREQUENCY_DAILY));
+
+        Person::orderBy('id')->chunk(100, function ($people) use ($sitemap) {
+            $people->each(
+                fn ($person) => $sitemap->add(
+                    Url::create('/people/'.$person->id)
+                        ->setLastModificationDate($person->updated_at)
+                        ->setChangeFrequency(Url::CHANGE_FREQUENCY_WEEKLY)
+                )
+            );
+        });
+
+        $sitemap->writeToFile(public_path('sitemap.xml'));
+    }
+}
