a29acd1ce428f7c965dfb60a96d4ef0682e6c373
maksiuP <maksiu@npng.pl>
2020-05-23 03:54:29 +0200

    improve main menu

--- resources/views/components/menu.blade.php
+++ resources/views/components/menu.blade.php
@@ -33,7 +33,7 @@
                 @if(request()->route()->getName() != 'search')
                     <form action="{{ route('search') }}"
                         class="relative mx-2 mb-2 lg:mb-0">
-                        <input type="search" name="s" required></input>
+                        <input type="search" name="s" required class="lg:text-sm"></input>
                         <button class="absolute inset-y-0 right-0 flex items-center px-2 text-gray-700 hover:text-gray-900 transition-colors duration-200 ease-out">
                             <svg class="fill-current h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
                                 <path d="M12.9 14.32a8 8 0 1 1 1.41-1.41l5.35 5.33-1.42 1.42-5.33-5.34zM8 14A6 6 0 1 0 8 2a6 6 0 0 0 0 12z"/>
