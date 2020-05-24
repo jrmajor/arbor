2c5d6b55eb1d7309c26656af2737854bc448d0c8
maksiuP <maksiu@npng.pl>
2020-05-25 00:43:41 +0200

    ui fix

--- resources/views/people/person.blade.php
+++ resources/views/people/person.blade.php
@@ -438,7 +438,7 @@
             <div class="w-full flex text-sm">
                 <div class="w-1/2 flex flex-col md:flex-row justify-center">
                     @if($person->father)
-                        <div class="w-full md:w-1/2 text-right">
+                        <div class="w-full md:w-1/2 md:text-right">
                             @if($person->father->father)
                                 <x-name :person="$person->father->father" :years="false"/>
                             @endif
@@ -446,7 +446,7 @@
                         @if($person->father->father && $person->father->mother)
                             <div class="mx-1">+</div>
                         @endif
-                        <div class="w-full md:w-1/2 text-left">
+                        <div class="w-full md:w-1/2 md:text-left">
                             @if($person->father->mother)
                                 <x-name :person="$person->father->mother" :years="false"/>
                             @endif
@@ -455,7 +455,7 @@
                 </div>
                 <div class="w-1/2 flex flex-col md:flex-row justify-center">
                     @if($person->mother)
-                        <div class="w-full md:w-1/2 text-right">
+                        <div class="w-full md:w-1/2 md:text-right">
                             @if($person->mother->father)
                                 <x-name :person="$person->mother->father" :years="false"/>
                             @endif
@@ -463,7 +463,7 @@
                         @if($person->mother->father && $person->mother->mother)
                             <div class="mx-1">+</div>
                         @endif
-                        <div class="w-full md:w-1/2 text-left">
+                        <div class="w-full md:w-1/2 md:text-left">
                             @if($person->mother->mother)
                                 <x-name :person="$person->mother->mother" :years="false"/>
                             @endif
