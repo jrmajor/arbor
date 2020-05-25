d6e510746fdc1ea18931295fb72eeee09860851f
maksiuP <maksiu@npng.pl>
2020-05-25 23:06:32 +0200

    bug fix

--- resources/views/components/history/td-date.blade.php
+++ resources/views/components/history/td-date.blade.php
@@ -5,17 +5,21 @@
     <tr>
         <td class="pr-5">{!! $label ?? __($activity['model']->log_name.'.'.$attribute) !!}</td>
         @if($activity['old'])
-            <td>
-                {{
-                    format_date_from_period(
-                        Carbon\Carbon::create($activity['old'][$attribute.'_from']),
-                        Carbon\Carbon::create($activity['old'][$attribute.'_to'])
-                    )
-                }}
-            </td>
+            @unless($activity['old'][$attribute.'_from'] === null && $activity['old'][$attribute.'_to'] === null)
+                <td>
+                    {{
+                        format_date_from_period(
+                            Carbon\Carbon::create($activity['old'][$attribute.'_from']),
+                            Carbon\Carbon::create($activity['old'][$attribute.'_to'])
+                        )
+                    }}
+                </td>
+            @else
+                <td class="text-gray-500">null</td>
+            @endif
             <td>=></td>
         @endif
-        @if($activity['attributes'][$attribute.'_from'] !== null)
+        @unless($activity['attributes'][$attribute.'_from'] === null && $activity['attributes'][$attribute.'_to'] === null)
             <td>
                 {{
                     format_date_from_period(
