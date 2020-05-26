976e8ebc528e6f1ebb1821b788023f41fc1a82c2
maksiuP <maksiu@npng.pl>
2020-05-26 02:13:10 +0200

    fix date tuples validation

--- app/Http/Requests/StoreMarriage.php
+++ app/Http/Requests/StoreMarriage.php
@@ -23,18 +23,48 @@ class StoreMarriage extends FormRequest
             'man_id' => 'required|integer|exists:people,id',
             'man_order' => 'integer|nullable',
             'rite' => [new EnumRule(MarriageRiteEnum::class), 'nullable'],
+
             'first_event_type' => [new EnumRule(MarriageEventTypeEnum::class), 'nullable'],
-            'second_event_type' => [new EnumRule(MarriageEventTypeEnum::class), 'nullable'],
-            'first_event_date_from' => ['date_format:Y-m-d', 'required_with:first_event_date_to', 'nullable'],
-            'first_event_date_to' => ['date_format:Y-m-d', 'after_or_equal:first_event_date_from', 'nullable'],
-            'second_event_date_from' => ['date_format:Y-m-d', 'required_with:second_event_date_to', 'nullable'],
-            'second_event_date_to' => ['date_format:Y-m-d', 'after_or_equal:second_event_date_from', 'nullable'],
+            'first_event_date_from' => [
+                'date_format:Y-m-d',
+                'required_with:first_event_date_to',
+                'nullable'
+            ],
+            'first_event_date_to' => [
+                'date_format:Y-m-d',
+                'required_with:first_event_date_from',
+                'after_or_equal:first_event_date_from',
+                'nullable'
+            ],
             'first_event_place' => 'string|max:100|nullable',
+
+            'second_event_type' => [new EnumRule(MarriageEventTypeEnum::class), 'nullable'],
+            'second_event_date_from' => [
+                'date_format:Y-m-d',
+                'required_with:second_event_date_to',
+                'nullable'
+            ],
+            'second_event_date_to' => [
+                'date_format:Y-m-d',
+                'required_with:second_event_date_from',
+                'after_or_equal:second_event_date_from',
+                'nullable'
+            ],
             'second_event_place' => 'string|max:100|nullable',
+
             'ended' => 'boolean',
             'end_cause' => 'string|max:100|nullable',
-            'end_date_from' => ['date_format:Y-m-d', 'required_with:end_date_to', 'nullable'],
-            'end_date_to' => ['date_format:Y-m-d', 'after_or_equal:end_date_from', 'nullable'],
+            'end_date_from' => [
+                'date_format:Y-m-d',
+                'required_with:end_date_to',
+                'nullable'
+            ],
+            'end_date_to' => [
+                'date_format:Y-m-d',
+                'required_with:first_event_date_from',
+                'after_or_equal:end_date_from',
+                'nullable'
+            ],
         ];
     }
 }
--- app/Http/Requests/StorePerson.php
+++ app/Http/Requests/StorePerson.php
@@ -17,26 +17,68 @@ class StorePerson extends FormRequest
         return [
             'id_wielcy' => 'string|max:20|nullable',
             'id_pytlewski' => 'integer|nullable',
-            'sex' => ['regex:/x(y|x)/', 'nullable'],
+
+            'sex' => 'in:xx,xy|nullable',
             'name' => 'required|string|max:100',
             'middle_name' => 'string|max:100|nullable',
             'family_name' => 'required|string|max:100',
             'last_name' => 'string|max:100|nullable',
+
             'mother_id' => 'integer|exists:people,id|nullable',
             'father_id' => 'integer|exists:people,id|nullable',
-            'birth_date_from' => ['date_format:Y-m-d', 'required_with:birth_date_to', 'nullable'],
-            'birth_date_to' => ['date_format:Y-m-d', 'after_or_equal:birth_date_from', 'nullable'],
+
+            'birth_date_from' => [
+                'date_format:Y-m-d',
+                'required_with:birth_date_to',
+                'nullable'
+            ],
+            'birth_date_to' => [
+                'date_format:Y-m-d',
+                'required_with:birth_date_from',
+                'after_or_equal:birth_date_from',
+                'nullable'
+            ],
             'birth_place' => 'string|max:100|nullable',
+
             'dead' => 'boolean',
-            'death_date_from' => ['date_format:Y-m-d', 'required_with:death_date_to', 'nullable'],
-            'death_date_to' => ['date_format:Y-m-d', 'after_or_equal:death_date_from', 'nullable'],
+            'death_date_from' => [
+                'date_format:Y-m-d',
+                'required_with:death_date_to',
+                'nullable'
+            ],
+            'death_date_to' => [
+                'date_format:Y-m-d',
+                'required_with:death_date_from',
+                'after_or_equal:death_date_from',
+                'nullable'
+            ],
             'death_place' => 'string|max:100|nullable',
             'death_cause' => 'string|max:100|nullable',
-            'funeral_date_from' => ['date_format:Y-m-d', 'required_with:funeral_date_to', 'nullable'],
-            'funeral_date_to' => ['date_format:Y-m-d', 'after_or_equal:funeral_date_from', 'nullable'],
+
+            'funeral_date_from' => [
+                'date_format:Y-m-d',
+                'required_with:funeral_date_to',
+                'nullable'
+            ],
+            'funeral_date_to' => [
+                'date_format:Y-m-d',
+                'required_with:funeral_date_from',
+                'after_or_equal:funeral_date_from',
+                'nullable'
+            ],
             'funeral_place' => 'string|max:100|nullable',
-            'burial_date_from' => ['date_format:Y-m-d', 'required_with:burial_date_to', 'nullable'],
-            'burial_date_to' => ['date_format:Y-m-d', 'after_or_equal:burial_date_from', 'nullable'],
+
+            'burial_date_from' => [
+                'date_format:Y-m-d',
+                'required_with:burial_date_to',
+                'nullable'
+            ],
+            'burial_date_to' => [
+                'date_format:Y-m-d',
+                'required_with:burial_date_from',
+                'after_or_equal:burial_date_from',
+                'nullable'
+            ],
             'burial_place' => 'string|max:100|nullable',
         ];
     }
