caee3b8049b4588a63bc3f91c321bd5b7502737f
maksiuP <maksiu@npng.pl>
2020-06-10 02:11:56 +0200

    bug fix

--- resources/views/components/pagination-links.blade.php
+++ resources/views/components/pagination-links.blade.php
@@ -1,49 +1,47 @@
 @if ($paginator->hasPages())
     <ul class="flex justify-center lg:justify-start flex-wrap space-x-3 -my-1" role="navigation">
-        {{-- Previous Page Link --}}
+
         @if ($paginator->onFirstPage())
             <li class="py-1">
-                <button class="btn" type="button" disabled>
+                <button id="pg-previous-disabled" class="btn" type="button" disabled>
                     &lsaquo;
                 </button>
             </li>
         @else
             <li class="py-1">
-                <button class="btn" type="button" wire:click="previousPage" rel="prev">
+                <button id="pg-previous" class="btn" type="button" wire:click="previousPage" rel="prev">
                     &lsaquo;
                 </button>
             </li>
         @endif
 
-        {{-- Pagination Elements --}}
         @foreach ($elements as $element)
-            {{-- "Three Dots" Separator --}}
+
             @if (is_string($element))
                 <li class="py-1"><span class="-m-1">{{ $element }}</span></li>
             @endif
 
-            {{-- Array Of Links --}}
             @if (is_array($element))
                 @foreach ($element as $page => $url)
                     @if ($page == $paginator->currentPage())
-                        <li class="py-1"><button class="btn" type="button" disabled>{{ $page }}</button></li>
+                        <li class="py-1"><button id="pg-current" class="btn" type="button" disabled>{{ $page }}</button></li>
                     @else
-                        <li class="py-1"><button class="btn" type="button" wire:click="gotoPage({{ $page }})">{{ $page }}</button></li>
+                        <li class="py-1"><button id="pg-{{ $page }}" class="btn" type="button" wire:click="gotoPage({{ $page }})">{{ $page }}</button></li>
                     @endif
                 @endforeach
             @endif
+
         @endforeach
 
-        {{-- Next Page Link --}}
         @if ($paginator->hasMorePages())
             <li class="py-1">
-                <button class="btn" type="button" wire:click="nextPage" rel="next">
+                <button id="pg-next" class="btn" type="button" wire:click="nextPage" rel="next">
                     &rsaquo;
                 </button>
             </li>
         @else
             <li class="py-1">
-                <button class="btn" type="button" disabled>
+                <button id="pg-next-disabled" class="btn" type="button" disabled>
                     &rsaquo;
                 </button>
             </li>
