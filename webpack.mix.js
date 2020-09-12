73e720974271e9128c9ef091eabff1f2422350bf
maksiuP <maksiu@npng.pl>
2020-09-12 16:21:32 +0200

    fix indentation

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,10 +1,9 @@
 const mix = require('laravel-mix')
 const tailwindcss = require('tailwindcss')
-require('laravel-mix-purgecss')
 
 mix.js('resources/js/app.js', 'public/js')
 
 mix.postCss('resources/css/app.css', 'public/css')
-    .options({
-        postCss: [ tailwindcss('tailwind.config.js') ],
-    }).version()
+  .options({
+    postCss: [ tailwindcss('tailwind.config.js') ],
+  }).version()
