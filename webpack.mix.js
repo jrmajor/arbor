368c2f02548c3d0e92d98bad7cb478311b782206
maksiuP <maksiu@npng.pl>
2020-10-26 00:14:18 +0100

    add js bundle analyzer

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,12 +2,21 @@ const mix = require('laravel-mix')
 const path = require('path')
 const tailwindcss = require('tailwindcss')
 
+require('laravel-mix-bundle-analyzer')
+
 mix.js('resources/js/app.js', 'public/js')
 
 mix.postCss('resources/css/app.css', 'public/css')
   .options({
     postCss: [tailwindcss('tailwind.config.js')]
   }).version()
+
+if (!mix.inProduction()) {
+  mix.bundleAnalyzer({
+    analyzerMode: 'static',
+    openAnalyzer: false
+  })
+}
 
 mix.webpackConfig({
   resolve: {
