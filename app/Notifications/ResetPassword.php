7499e45b73d7cf037ce3f7e4538d60a0861d5fcb
maksiuP <jrh.mjr@gmail.com>
2020-11-27 05:20:46 +0100

    remove redundant docblocks

--- app/Notifications/ResetPassword.php
+++ app/Notifications/ResetPassword.php
@@ -8,55 +8,21 @@ use Illuminate\Support\Facades\Lang;
 
 class ResetPassword extends Notification
 {
-    /**
-     * The password reset token.
-     *
-     * @var string
-     */
-    public $token;
 
-    /**
-     * The callback that should be used to create the reset password URL.
-     *
-     * @var \Closure|null
-     */
     public static $createUrlCallback;
 
-    /**
-     * The callback that should be used to build the mail message.
-     *
-     * @var \Closure|null
-     */
     public static $toMailCallback;
 
-    /**
-     * Create a notification instance.
-     *
-     * @param  string  $token
-     * @return void
-     */
     public function __construct($token)
     {
         $this->token = $token;
     }
 
-    /**
-     * Get the notification's channels.
-     *
-     * @param  mixed  $notifiable
-     * @return array|string
-     */
     public function via($notifiable)
     {
         return ['mail'];
     }
 
-    /**
-     * Build the mail representation of the notification.
-     *
-     * @param  mixed  $notifiable
-     * @return \Illuminate\Notifications\Messages\MailMessage
-     */
     public function toMail($notifiable)
     {
         if (static::$toMailCallback) {
@@ -80,23 +46,11 @@ class ResetPassword extends Notification
             ->line(Lang::get('passwords.if_you_didnt_request'));
     }
 
-    /**
-     * Set a callback that should be used when creating the reset password button URL.
-     *
-     * @param  \Closure  $callback
-     * @return void
-     */
     public static function createUrlUsing($callback)
     {
         static::$createUrlCallback = $callback;
     }
 
-    /**
-     * Set a callback that should be used when building the notification mail message.
-     *
-     * @param  \Closure  $callback
-     * @return void
-     */
     public static function toMailUsing($callback)
     {
         static::$toMailCallback = $callback;
--- app/Observers/PersonObserver.php
+++ app/Observers/PersonObserver.php
@@ -7,56 +7,26 @@ use Illuminate\Support\Facades\Cache;
 
 class PersonObserver
 {
-    /**
-     * Handle the person "created" event.
-     *
-     * @param  \App\Person  $person
-     * @return void
-     */
     public function created(Person $person)
     {
         $this->clearLettersCache();
     }
 
-    /**
-     * Handle the person "updated" event.
-     *
-     * @param  \App\Person  $person
-     * @return void
-     */
     public function updated(Person $person)
     {
         $this->clearLettersCache();
     }
 
-    /**
-     * Handle the person "deleted" event.
-     *
-     * @param  \App\Person  $person
-     * @return void
-     */
     public function deleted(Person $person)
     {
         $this->clearLettersCache();
     }
 
-    /**
-     * Handle the person "restored" event.
-     *
-     * @param  \App\Person  $person
-     * @return void
-     */
     public function restored(Person $person)
     {
         $this->clearLettersCache();
     }
 
-    /**
-     * Handle the person "force deleted" event.
-     *
-     * @param  \App\Person  $person
-     * @return void
-     */
     public function forceDeleted(Person $person)
     {
         $this->clearLettersCache();
