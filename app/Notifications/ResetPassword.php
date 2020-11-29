8199a1821a0f5a2612e905ba0d3ec84f42e5c90a
maksiuP <jrh.mjr@gmail.com>
2020-11-29 01:58:55 +0100

    fix password reset notification

--- app/Notifications/ResetPassword.php
+++ app/Notifications/ResetPassword.php
@@ -4,10 +4,11 @@ namespace App\Notifications;
 
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
-use Illuminate\Support\Facades\Lang;
 
 class ResetPassword extends Notification
 {
+    public string $token;
+
     public function __construct($token)
     {
         $this->token = $token;
@@ -26,10 +27,12 @@ class ResetPassword extends Notification
         ], false));
 
         return (new MailMessage)
-            ->subject(Lang::get('passwords.reset_password'))
-            ->line(Lang::get('passwords.you_are_recieving'))
-            ->action(Lang::get('passwords.reset_password'), $url)
-            ->line(Lang::get('passwords.reset_link_will_expire', ['count' => config('auth.passwords.'.config('auth.defaults.passwords').'.expire')]))
-            ->line(Lang::get('passwords.if_you_didnt_request'));
+            ->subject(__('passwords.reset_password'))
+            ->line(__('passwords.you_are_recieving'))
+            ->action(__('passwords.reset_password'), $url)
+            ->line(__('passwords.reset_link_will_expire', [
+                'count' => config('auth.passwords.'.config('auth.defaults.passwords').'.expire')
+            ]))
+            ->line(__('passwords.if_you_didnt_request'));
     }
 }
