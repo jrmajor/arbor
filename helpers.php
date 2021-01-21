95cd1d1b698018d2a56f67993cb741d5782dd47d
Jeremiasz Major <jrh.mjr@gmail.com>
2021-01-21 18:17:57 +0100

    add type hints to roman helper

--- helpers.php
+++ helpers.php
@@ -39,10 +39,26 @@ function format_date_from_period(Carbon $from, Carbon $to): string
     return __('misc.date.between').' '.$from->toDateString().' '.__('misc.date.and').' '.$to->toDateString();
 }
 
-function roman($number)
+function roman(int $number): string
 {
-    $map = ['M' => 1000, 'CM' => 900, 'D' => 500, 'CD' => 400, 'C' => 100, 'XC' => 90, 'L' => 50, 'XL' => 40, 'X' => 10, 'IX' => 9, 'V' => 5, 'IV' => 4, 'I' => 1];
+    $map = [
+        'M' => 1000,
+        'CM' => 900,
+        'D' => 500,
+        'CD' => 400,
+        'C' => 100,
+        'XC' => 90,
+        'L' => 50,
+        'XL' => 40,
+        'X' => 10,
+        'IX' => 9,
+        'V' => 5,
+        'IV' => 4,
+        'I' => 1,
+    ];
+
     $returnValue = '';
+
     while ($number > 0) {
         foreach ($map as $roman => $int) {
             if ($number >= $int) {
