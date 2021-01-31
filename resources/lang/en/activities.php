63adb982293d82d44080dc0800bde06d0af9f1be
Jeremiasz Major <jrh.mjr@gmail.com>
2021-01-31 19:48:10 +0100

    add missing translations

--- resources/lang/en/activities.php
+++ resources/lang/en/activities.php
@@ -7,6 +7,7 @@ return [
     'made_visible' => 'made visible',
     'made_invisible' => 'made invisible',
     'deleted' => 'deleted',
+    'restored' => 'restored',
     'added-biography' => 'had biography added',
     'updated-biography' => 'had biography updated',
     'deleted-biography' => 'had biography deleted',
--- resources/lang/pl/activities.php
+++ resources/lang/pl/activities.php
@@ -7,6 +7,7 @@ return [
     'made_visible' => 'opublicznion',
     'made_invisible' => 'sprywatyzowan',
     'deleted' => 'usunion',
+    'restored' => 'przywrócon',
     'added-biography' => 'wzbogaciła się o biografię',
     'updated-biography' => 'otrzymała zmodyfikowaną biografię',
     'deleted-biography' => 'zubożała o biografię',
