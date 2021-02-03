342df7ec30f984e348bd5b99b8763ad53780d9db
Jeremiasz Major <jrh.mjr@gmail.com>
2021-02-03 14:58:18 +0100

    do not show links to hidden people in pytlewski data

--- resources/views/components/pytlewski-relative.blade.php
+++ resources/views/components/pytlewski-relative.blade.php
@@ -1,11 +1,17 @@
 @if ($pytlewski->person)
-    <a
-        href="{{ route('people.show', $pytlewski->person) }}"
-        class="text-green-600 hover:text-green-800">
+    @if ($pytlewski->person->canBeViewedBy(auth()->user()))
+        <a
+            href="{{ route('people.show', $pytlewski->person) }}"
+            class="text-green-600 hover:text-green-700">
+    @else
+        <a
+            target="_blank" href="{{ $pytlewski->url }}"
+            class="text-yellow-600 hover:text-yellow-700">
+    @endif
 @elseif ($pytlewski->id)
     <a
         target="_blank" href="{{ $pytlewski->url }}"
-        class="text-red-600 hover:text-red-800">
+        class="text-red-600 hover:text-red-700">
 @endif
 
     @if($pytlewski->surname)
