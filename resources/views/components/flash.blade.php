da4c2d0700ab5a376b29db69ab423dedab6dca00
Jeremiasz Major <jrh.mjr@gmail.com>
2021-02-13 00:59:20 +0100

    formatting

--- resources/views/components/flash.blade.php
+++ resources/views/components/flash.blade.php
@@ -1,21 +1,18 @@
 @if(flash()->message)
     <div
         class="w-full rounded-lg shadow p-5 flex items-center mb-3
-        @if(flash()->level === 'error')
-            bg-red-100 text-red-900
-        @elseif(flash()->level === 'warning')
-            bg-yellow-100 text-yellow-900
-        @elseif(flash()->level === 'success')
-            bg-green-100 text-green-900
-        @endif ">
+        {{ match(flash()->level) {
+            'error' => 'bg-red-100 text-red-900',
+            'warning' => 'bg-yellow-100 text-yellow-900',
+            'success' => 'bg-green-100 text-green-900',
+        } }}">
         <svg class="flex-none w-5 h-5 mr-5 fill-current
-            @if(flash()->level === 'error')
-                text-red-500
-            @elseif(flash()->level === 'warning')
-                text-yellow-500
-            @elseif(flash()->level === 'success')
-                text-green-500
-            @endif " viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+            {{ match(flash()->level) {
+                'error' => 'text-red-500',
+                'warning' => 'text-yellow-500',
+                'success' => 'text-green-500',
+            } }}"
+            viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
             @if(flash()->level === 'error')
                 <path d="M2.93 17.07A10 10 0 1 1 17.07 2.93 10 10 0 0 1 2.93 17.07zM11.4 10l2.83-2.83-1.41-1.41L10 8.59 7.17 5.76 5.76 7.17 8.59 10l-2.83 2.83 1.41 1.41L10 11.41l2.83 2.83 1.41-1.41L11.41 10z"/> --}}
             @elseif(flash()->level === 'warning')
