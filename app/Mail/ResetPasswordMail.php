bc3dcca99bdf4e99c55f716d2e33f117e83d7b21
Jeremiasz Major <jrh.mjr@gmail.com>
2021-03-17 20:26:19 +0100

    formatting

--- app/Mail/ResetPasswordMail.php
+++ app/Mail/ResetPasswordMail.php
@@ -8,7 +8,8 @@ use Illuminate\Queue\SerializesModels;
 
 class ResetPasswordMail extends Mailable
 {
-    use Queueable, SerializesModels;
+    use Queueable;
+    use SerializesModels;
 
     public function __construct(
         protected string $resetLink,
