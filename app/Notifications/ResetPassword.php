ea58af9f8187fa7b5918d5aa286f1d35a41762ef
Jeremiasz Major <jrh.mjr@gmail.com>
2021-03-18 10:52:01 +0100

    simplify

--- app/Notifications/ResetPassword.php
+++ app/Notifications/ResetPassword.php
@@ -2,8 +2,8 @@
 
 namespace App\Notifications;
 
-use App\Mail\ResetPasswordMail;
 use App\Models\User;
+use Illuminate\Bus\Queueable;
 use Illuminate\Mail\Mailable;
 use Illuminate\Notifications\Notification;
 
@@ -25,7 +25,25 @@ class ResetPassword extends Notification
             'email' => $notifiable->getEmailForPasswordReset(),
         ], false));
 
-        return (new ResetPasswordMail($url))
-            ->to($notifiable->email);
+        return new class ($notifiable->email, $url) extends Mailable {
+            use Queueable;
+
+            public function __construct(
+                string $to,
+                protected string $url,
+            ) {
+                $this->to($to);
+            }
+
+            public function build()
+            {
+                return $this
+                    ->subject(__('passwords.password_reset'))
+                    ->text(
+                        'emails.reset-password',
+                        ['url' => $this->url],
+                    );
+            }
+        };
     }
 }
