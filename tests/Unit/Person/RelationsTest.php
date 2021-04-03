5f1a6a6019988a62f3ae85b468e8db5832d0d78d
Jeremiasz Major <jrh.mjr@gmail.com>
2021-04-03 20:03:59 +0200

    remove unused test

--- tests/Unit/Person/RelationsTest.php
+++ tests/Unit/Person/RelationsTest.php
@@ -16,39 +16,3 @@ it('can get father', function () {
 
     expect($person->father->id)->toBe($father->id);
 });
-
-it('can get partners', function () {
-    $person = Person::factory()->woman()->create();
-
-    $spouse = Person::factory()->man()->create(['name' => 'Spouse']);
-
-    Marriage::factory()->create([
-        'woman_id' => $person->id,
-        'man_id' => $spouse->id,
-    ]);
-
-    $spouseWithChild = Person::factory()->man()->create(['name' => 'Spouse With Child']);
-
-    Marriage::factory()->create([
-        'woman_id' => $person->id,
-        'man_id' => $spouseWithChild->id,
-    ]);
-
-    Person::factory()->create([
-        'mother_id' => $person->id,
-        'father_id' => $spouseWithChild->id,
-    ]);
-
-    $lover = Person::factory()->man()->create(['name' => 'Lover']);
-
-    Person::factory()->create([
-        'mother_id' => $person->id,
-        'father_id' => $lover->id,
-    ]);
-
-    expect($person->partners())->toHaveCount(3);
-
-    expect($person->partners()->contains($spouse))->toBeTrue();
-    expect($person->partners()->contains($spouseWithChild))->toBeTrue();
-    expect($person->partners()->contains($lover))->toBeTrue();
-})->skip();
