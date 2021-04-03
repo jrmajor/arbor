c6328f839c12d584e87dd33107f7664737d58025
Jeremiasz Major <jrh.mjr@gmail.com>
2021-04-03 21:41:27 +0200

    PersonFactory formatting

--- database/factories/PersonFactory.php
+++ database/factories/PersonFactory.php
@@ -7,7 +7,7 @@ use Carbon\Carbon;
 use Carbon\CarbonInterval;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
-class PersonFactory extends Factory
+final class PersonFactory extends Factory
 {
     protected $model = Person::class;
 
@@ -23,24 +23,22 @@ class PersonFactory extends Factory
             'family_name' => $this->faker->lastName($sex),
             'last_name' => $sex === 'female' && $this->faker->boolean ? $this->faker->lastName($sex) : null,
             'birth_date_from' => $this->faker->dateTimeBetween('-80 years', '-30 years')->format('Y-m-d'),
-            'birth_date_to' => fn ($person) => $person['birth_date_from'],
+            'birth_date_to' => fn ($p) => $p['birth_date_from'],
             'birth_place' => $this->faker->city.', Polska',
             'dead' => $dead,
             'death_date_from' => $dead ? $this->faker->dateTimeBetween('-29 years', '-5 years')->format('Y-m-d') : null,
-            'death_date_to' => fn ($person) => $person['death_date_from'],
+            'death_date_to' => fn ($p) => $p['death_date_from'],
             'death_place' => $dead ? $this->faker->city.', Polska' : null,
-            'funeral_date_from' => function ($person) use ($dead) {
-                return $dead
-                    ? (new Carbon($person['death_date_from']))
-                        ->add(CarbonInterval::days(5))
-                        ->format('Y-m-d')
-                    : null;
-            },
-            'funeral_date_to' => fn ($person) => $person['funeral_date_from'],
-            'funeral_place' => fn ($person) => $person['death_place'],
-            'burial_date_from' => fn ($person) => $person['funeral_date_from'],
-            'burial_date_to' => fn ($person) => $person['burial_date_from'],
-            'burial_place' => fn ($person) => $person['death_place'],
+            'funeral_date_from' => fn ($p) => $dead
+                ? (new Carbon($p['death_date_from']))
+                    ->add(CarbonInterval::days(5))
+                    ->format('Y-m-d')
+                : null,
+            'funeral_date_to' => fn ($p) => $p['funeral_date_from'],
+            'funeral_place' => fn ($p) => $p['death_place'],
+            'burial_date_from' => fn ($p) => $p['funeral_date_from'],
+            'burial_date_to' => fn ($p) => $p['burial_date_from'],
+            'burial_place' => fn ($p) => $p['death_place'],
         ];
     }
 
@@ -60,33 +58,29 @@ class PersonFactory extends Factory
         ]);
     }
 
-    public function woman()
+    public function woman(): self
     {
-        return $this->state(function () {
-            return [
-                'sex' => 'xx',
-                'name' => $this->faker->firstName('female'),
-                'middle_name' => $this->faker->boolean ? $this->faker->firstName('female') : null,
-                'family_name' => $this->faker->lastName('female'),
-                'last_name' => $this->faker->boolean ? $this->faker->lastName('female') : null,
-            ];
-        });
+        return $this->state([
+            'sex' => 'xx',
+            'name' => $this->faker->firstName('female'),
+            'middle_name' => $this->faker->boolean ? $this->faker->firstName('female') : null,
+            'family_name' => $this->faker->lastName('female'),
+            'last_name' => $this->faker->boolean ? $this->faker->lastName('female') : null,
+        ]);
     }
 
-    public function man()
+    public function man(): self
     {
-        return $this->state(function () {
-            return [
-                'sex' => 'xy',
-                'name' => $this->faker->firstName('male'),
-                'middle_name' => $this->faker->boolean ? $this->faker->firstName('male') : null,
-                'family_name' => $this->faker->lastName('male'),
-                'last_name' => null,
-            ];
-        });
+        return $this->state([
+            'sex' => 'xy',
+            'name' => $this->faker->firstName('male'),
+            'middle_name' => $this->faker->boolean ? $this->faker->firstName('male') : null,
+            'family_name' => $this->faker->lastName('male'),
+            'last_name' => null,
+        ]);
     }
 
-    public function alive()
+    public function alive(): self
     {
         return $this->state([
             'dead' => false,
@@ -102,25 +96,23 @@ class PersonFactory extends Factory
         ]);
     }
 
-    public function dead()
+    public function dead(): self
     {
-        return $this->state(function ($person) {
-            return [
-                'dead' => true,
-                'death_date_from' => $this->faker->dateTimeBetween('-29 years', '-5 years')->format('Y-m-d'),
-                'death_date_to' => fn ($person) => $person['death_date_from'],
-                'death_place' => $this->faker->city.', Polska',
-                'funeral_date_from' => function ($person) {
-                    return (new Carbon($person['death_date_from']))
-                        ->add(CarbonInterval::days(5))
-                        ->format('Y-m-d');
-                },
-                'funeral_date_to' => fn ($person) => $person['funeral_date_from'],
-                'funeral_place' => fn ($person) => $person['death_place'],
-                'burial_date_from' => fn ($person) => $person['funeral_date_from'],
-                'burial_date_to' => fn ($person) => $person['burial_date_from'],
-                'burial_place' => fn ($person) => $person['death_place'],
-            ];
-        });
+        return $this->state([
+            'dead' => true,
+            'death_date_from' => $this->faker->dateTimeBetween('-29 years', '-5 years')->format('Y-m-d'),
+            'death_date_to' => fn ($p) => $p['death_date_from'],
+            'death_place' => $this->faker->city.', Polska',
+            'funeral_date_from' => function ($person) {
+                return (new Carbon($person['death_date_from']))
+                    ->add(CarbonInterval::days(5))
+                    ->format('Y-m-d');
+            },
+            'funeral_date_to' => fn ($p) => $p['funeral_date_from'],
+            'funeral_place' => fn ($p) => $p['death_place'],
+            'burial_date_from' => fn ($p) => $p['funeral_date_from'],
+            'burial_date_to' => fn ($p) => $p['burial_date_from'],
+            'burial_place' => fn ($p) => $p['death_place'],
+        ]);
     }
 }
