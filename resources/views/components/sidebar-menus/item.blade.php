e98f57a9da817679fa780d2c9641f95f77718b20
Jeremiasz Major <jrh.mjr@gmail.com>
2021-04-18 14:41:30 +0200

    add distinct focus state for sidebar menu item

--- resources/views/components/sidebar-menus/item.blade.php
+++ resources/views/components/sidebar-menus/item.blade.php
@@ -29,11 +29,11 @@
       {{ $danger ? 'text-red-600 hover:text-red-700 focus:text-red-700' : 'text-gray-700 hover:text-gray-800 focus:text-gray-800' }}"
   >
     <li class="px-3 py-1 rounded transition
-      {{ $danger ? 'group-hover:bg-red-200 group-focus:bg-red-300' : 'group-hover:bg-gray-200 group-focus:bg-gray-200' }}"
+      {{ $danger ? 'group-hover:bg-red-200 group-focus:bg-red-300' : 'group-hover:bg-gray-200 group-focus:bg-gray-300' }}"
     >
       <span class="w-full flex items-center">
         <svg class="h-4 w-4 mr-2 fill-current transition
-          @if (! $danger) text-gray-600 group-hover:text-gray-700 group-focus:text-gray-700 @endif"
+          @unless ($danger) text-gray-600 group-hover:text-gray-700 group-focus:text-gray-700 @endif"
           viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"
         >
           {{ $slot }}
