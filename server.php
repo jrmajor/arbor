8189f12a066301eaa806b43e03ab04db77b67cc6
Jeremiasz Major <jrh.mjr@gmail.com>
2021-04-30 02:19:16 +0200

    formatting

--- server.php
+++ server.php
@@ -1,7 +1,7 @@
 <?php
 
 $uri = urldecode(
-    parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH)
+    parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH),
 );
 
 if ($uri !== '/' && file_exists(__DIR__.'/public'.$uri)) {
