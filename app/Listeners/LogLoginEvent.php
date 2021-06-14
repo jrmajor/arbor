0ee90ffb0c200c08a6203f673dc1e3d298177b9e
Jeremiasz Major <jrh.mjr@gmail.com>
2021-06-14 16:19:18 +0200

    bump phpstan level to 5

--- app/Listeners/LogLoginEvent.php
+++ app/Listeners/LogLoginEvent.php
@@ -2,13 +2,19 @@
 
 namespace App\Listeners;
 
+use App\Models\User;
 use Illuminate\Auth\Events\Login;
 use Jenssegers\Agent\Facades\Agent;
+use TypeError;
 
 class LogLoginEvent
 {
     public function handle(Login $event)
     {
+        if (! $event->user instanceof User) {
+            throw new TypeError('$event->user should be instance of App\\Models\\User.');
+        }
+
         activity('logins')
             ->causedBy($event->user)
             ->withProperties($this->getAgent())
