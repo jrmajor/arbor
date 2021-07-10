cac5b6aa195f8342c0b82693a253449a970512c5
Jeremiasz Major <jrh.mjr@gmail.com>
2021-07-10 19:57:05 +0200

    remove unused import

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\LoginRequest;
-use Illuminate\Auth\SessionGuard;
 use Illuminate\Http\Request;
 
 use function App\Services\flash;
