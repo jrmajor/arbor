150cfc02d85801ce71fec71ffad748c7e6498352
Jeremiasz Major <jrh.mjr@gmail.com>
2021-09-06 21:35:33 +0200

    replace string rule "in" with method

--- app/Http/Requests/LocaleRequest.php
+++ app/Http/Requests/LocaleRequest.php
@@ -3,14 +3,15 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
 class LocaleRequest extends FormRequest
 {
     public function rules(): array
     {
-        $supportedLocales = implode(',', config('app.available_locales'));
-
-        return ['language' => ['required', "in:{$supportedLocales}"]];
+        return [
+            'language' => ['required', Rule::in(config('app.available_locales'))],
+        ];
     }
 
     public function locale(): string
