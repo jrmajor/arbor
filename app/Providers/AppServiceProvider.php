5e90ad683a70930cf734381457c7d143a9c75b59
Jeremiasz Major <jrh.mjr@gmail.com>
2021-12-19 03:51:39 +0100

    remove lazy loading prevention

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,6 @@
 namespace App\Providers;
 
 use App\Models;
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\Relation;
 use Illuminate\Support\ServiceProvider;
 use Spatie\Flash\Flash;
@@ -12,8 +11,6 @@ class AppServiceProvider extends ServiceProvider
 {
     public function boot(): void
     {
-        Model::preventLazyLoading(! $this->app->isProduction());
-
         Relation::enforceMorphMap([
             'marriage' => Models\Marriage::class,
             'person' => Models\Person::class,
