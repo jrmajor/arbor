e40be39a56a067a60b0aa20a23454da7f546fd26
Jeremiasz Major <jrh.mjr@gmail.com>
2021-12-19 05:23:28 +0100

    refactor laravel stringable to psl pipe

--- app/Services/helpers.php
+++ app/Services/helpers.php
@@ -2,23 +2,20 @@
 
 namespace App\Services;
 
-use Illuminate\Support\Str;
+use Psl\Fun;
+use Psl\Str;
 use Spatie\Flash\Flash;
 use Spatie\Flash\Message as FlashMessage;
 
 function formatBiography(?string $biography): ?string
 {
-    if ($biography === null) {
-        return null;
-    }
-
-    return (string) Str::of($biography)
-        ->trim()
-        ->replace(["\r\n", "\r"], "\n")
-        ->pipe(e(...))
-        ->prepend('<p>')
-        ->append('</p>')
-        ->replace("\n\n", "</p>\n<p>");
+    return $biography === null ? null : Fun\pipe(
+        fn ($s) => Str\trim($s),
+        fn ($s) => Str\replace_every($s, ["\r\n" => "\n", "\r" => "\n"]),
+        fn ($s) => e($s),
+        fn ($s) => "<p>{$s}</p>",
+        fn ($s) => Str\replace($s, "\n\n", "</p>\n<p>"),
+    )($biography);
 }
 
 function flash(string $class, string $text): void
