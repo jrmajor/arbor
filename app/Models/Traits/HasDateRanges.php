002d617fa233c768b82cf1858efa285095ef4705
Jeremiasz Major <jrh.mjr@gmail.com>
2022-01-02 20:08:22 +0100

    use psl in HasDateRanges trait

--- app/Models/Traits/HasDateRanges.php
+++ app/Models/Traits/HasDateRanges.php
@@ -5,20 +5,21 @@ namespace App\Models\Traits;
 use App\Models\Casts\DateRangeToDateCast;
 use App\Models\Casts\DateRangeToYearCast;
 use InvalidArgumentException;
+use Psl\Str;
 
 trait HasDateRanges
 {
     public function initializeHasDateRanges(): void
     {
         foreach (static::$dateRanges as $dateRange) {
-            if (! str_ends_with($dateRange, '_date')) {
+            if (! Str\ends_with($dateRange, '_date')) {
                 throw new InvalidArgumentException();
             }
 
             $this->casts["{$dateRange}_from"] = 'datetime:Y-m-d';
             $this->casts["{$dateRange}_to"] = 'datetime:Y-m-d';
 
-            $this->casts[str_replace('date', 'year', $dateRange)] = DateRangeToYearCast::class;
+            $this->casts[Str\replace($dateRange, 'date', 'year')] = DateRangeToYearCast::class;
             $this->casts[$dateRange] = DateRangeToDateCast::class;
         }
     }
