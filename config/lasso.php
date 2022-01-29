d7a9a728aa7273d4b6883f7d01234e24028bafa8
Jeremiasz Major <jrh.mjr@gmail.com>
2022-01-29 05:57:06 +0100

    upgrade sammyjo20/lasso to v2

--- config/lasso.php
+++ config/lasso.php
@@ -5,6 +5,7 @@ return [
     'compiler' => [
         'script' => 'yarn run production',
         'timeout' => 300,
+        'output' => 'progress',
         'excluded_files' => [],
         'excluded_directories' => [],
     ],
@@ -13,6 +14,7 @@ return [
         'disk' => 'assets',
         'upload_to' => 'lasso',
         'environment' => env('LASSO_ENV', null),
+        'prefix' => env('LASSO_PREFIX', ''),
         'max_bundles' => 5,
     ],
 
