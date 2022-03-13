1c883a44868c056f25f756d031612bae605b1625
Jeremiasz Major <jrh.mjr@gmail.com>
2022-03-13 15:10:45 +0100

    remove assets bucket

--- config/filesystems.php
+++ config/filesystems.php
@@ -28,16 +28,6 @@ return [
             'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
         ],
 
-        'assets' => [
-            'driver' => 's3',
-            'key' => env('S3_ACCESS_KEY_ID'),
-            'secret' => env('S3_SECRET_ACCESS_KEY'),
-            'region' => env('S3_DEFAULT_REGION'),
-            'bucket' => env('S3_ASSETS_BUCKET'),
-            'endpoint' => env('S3_ENDPOINT'),
-            'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
-        ],
-
         'backup' => [
             'driver' => 's3',
             'key' => env('BACKUP_S3_ACCESS_KEY_ID'),
