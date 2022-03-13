fcb33ebba5362627cea4b7a4a5a36c2318be362c
Jeremiasz Major <jrh.mjr@gmail.com>
2022-03-13 22:28:33 +0100

    convert feature/auth tests to phpunit and psl

--- tests/Feature/Console/GenerateSitemapTest.php
+++ tests/Feature/Console/GenerateSitemapTest.php
@@ -1,34 +1,52 @@
 <?php
 
+namespace Tests\Feature\Console;
+
 use App\Models\Person;
+use PHPUnit\Framework\Attributes\TestDox;
+use Psl\File;
+use Psl\Filesystem;
+use Tests\TestCase;
 
-it('can generate sitemap', function () {
-    $sitemapPath = __DIR__ . '/../../../public/sitemap.xml';
+final class GenerateSitemapTest extends TestCase
+{
+    private const SITEMAP_PATH = __DIR__ . '/../../../public/sitemap.xml';
 
-    if (file_exists($sitemapPath)) {
-        unlink($sitemapPath);
+    #[TestDox('it can generate sitemap')]
+    public function testSitemap(): void
+    {
+        if (Filesystem\exists(self::SITEMAP_PATH)) {
+            Filesystem\delete_file(self::SITEMAP_PATH);
+        }
+
+        $people = Person::factory()->createMany([
+            ['name' => 'Hidden'],
+            ['name' => 'Hidden'],
+            ['name' => 'Kajnacy Masnorowski', 'visibility' => true],
+            ['name' => 'Koxemiasz Kajnor', 'visibility' => true],
+        ]);
+
+        $this->artisan('sitemap:generate')
+            ->expectsOutput('Sitemap has been generated successfully.')
+            ->assertExitCode(0);
+
+        $this->assertFileExists(self::SITEMAP_PATH);
+
+        $sitemap = File\read(self::SITEMAP_PATH);
+
+        foreach ([
+            '<loc>http://arbor.test</loc>' => true,
+            '<loc>http://arbor.test/people</loc>' => true,
+            "<loc>http://arbor.test/people/{$people[0]->id}</loc>" => false,
+            "<loc>http://arbor.test/people/{$people[1]->id}</loc>" => false,
+            "<loc>http://arbor.test/people/{$people[2]->id}</loc>" => true,
+            "<loc>http://arbor.test/people/{$people[3]->id}</loc>" => true,
+        ] as $part => $present) {
+            if ($present) {
+                $this->assertStringContainsString($part, $sitemap);
+            } else {
+                $this->assertStringNotContainsString($part, $sitemap);
+            }
+        }
     }
-
-    expect($sitemapPath)->not->toBeFile();
-
-    $people = Person::factory(4)->sequence(
-        ['name' => 'Hidden'],
-        ['name' => 'Hidden'],
-        ['name' => 'Kajnacy Masnorowski', 'visibility' => true],
-        ['name' => 'Koxemiasz Kajnor', 'visibility' => true],
-    )->create();
-
-    $this->artisan('sitemap:generate')
-        ->expectsOutput('Sitemap has been generated successfully.')
-        ->assertExitCode(0);
-
-    expect($sitemapPath)->toBeFile();
-
-    expect(file_get_contents($sitemapPath))
-        ->toContain('<loc>http://arbor.test</loc>')
-        ->toContain('<loc>http://arbor.test/people</loc>')
-        ->not->toContain("<loc>http://arbor.test/people/{$people[0]->id}</loc>")
-        ->not->toContain("<loc>http://arbor.test/people/{$people[1]->id}</loc>")
-        ->toContain("<loc>http://arbor.test/people/{$people[2]->id}</loc>")
-        ->toContain("<loc>http://arbor.test/people/{$people[3]->id}</loc>");
-});
+}
