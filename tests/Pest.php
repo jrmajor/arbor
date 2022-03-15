c424efac903b9999c04d1dbc9b7054716499574a
Jeremiasz Major <jrh.mjr@gmail.com>
2022-03-15 22:24:46 +0100

    remove unused expectation

--- tests/Pest.php
+++ tests/Pest.php
@@ -1,25 +1,5 @@
 <?php
 
-use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\Relation;
-use PHPUnit\Framework\Assert;
-use PHPUnit\Framework\Constraint\IsInstanceOf;
 use Tests\TestCase;
 
 uses(TestCase::class)->in(__DIR__);
-
-expect()->extend('toBeModel', function ($expected) {
-    $this->toMatchConstraint(
-        Assert::logicalOr(
-            new IsInstanceOf(Model::class),
-            new IsInstanceOf(Relation::class),
-        ),
-    );
-
-    Assert::assertTrue(
-        $this->value->is($expected),
-        'Value is not expected Eloquent model.',
-    );
-
-    return $this;
-});
