c91aa75691fc771397d193a37bacba2a79ef9d7b
Jeremiasz Major <jrh.mjr@gmail.com>
2022-07-25 22:04:13 +0200

    update skeleton

--- app/Application.php
+++ app/Application.php
@@ -6,7 +6,7 @@ use Illuminate\Foundation\Application as BaseApplication;
 
 class Application extends BaseApplication
 {
-    public const SkeletonVersion = '9.2.0';
+    public const SkeletonVersion = '9.3.0';
 
-    public const BreezeVersion = '1.10.0';
+    public const BreezeVersion = '1.11.1';
 }
--- vite.config.js
+++ vite.config.js
@@ -3,19 +3,12 @@ import laravel from 'laravel-vite-plugin'
 
 export default defineConfig({
   plugins: [
-    laravel([
-      'resources/css/style.css',
-      'resources/js/app.ts',
-    ]),
-    {
-      name: 'blade',
-      handleHotUpdate({ file, server }) {
-        if (! file.endsWith('.blade.php')) {
-          return
-        }
-
-        server.ws.send({ type: 'full-reload', path: '*' })
-      },
-    },
+    laravel({
+      input: [
+        'resources/css/style.css',
+        'resources/js/app.ts',
+      ],
+      refresh: true,
+    }),
   ],
 })
