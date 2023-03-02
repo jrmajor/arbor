4ceb66906d83586143dae21f8664bdd0afbc5cc5
Jeremiasz Major <jrh.mjr@gmail.com>
2023-03-02 07:19:10 +0100

    make my email harder to scrape

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,8 +10,8 @@
 
     @yield('content')
 
-    <footer class="m-1 px-3 pt-2 text-center text-gray-600 text-sm">
-      &copy; 2018-{{ now()->year }} <a href="mailto:jeremiah.major@npng.pl">Jeremiasz Major</a>
+    <footer class="m-1 px-3 pt-2 text-center text-gray-600 text-sm" x-data>
+      &copy; 2018-{{ now()->year }} <a x-bind:href="'mailto:' + atob('anJoLm1qckBnbWFpbC5jb20=')">Jeremiasz Major</a>
     </footer>
 
   </div>
