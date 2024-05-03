28c3bf8780560fad043abbe2e84d5d2b5100a0eb
Jeremiasz Major <jrh.mjr@gmail.com>
2024-05-03 23:15:13 +0200

    remove laravel/slack-notification-channel

--- config/services.php
+++ config/services.php
@@ -19,13 +19,6 @@ return [
         'region' => env('AWS_DEFAULT_REGION', 'eu-central-1'),
     ],
 
-    'slack' => [
-        'notifications' => [
-            'bot_user_oauth_token' => env('SLACK_BOT_USER_OAUTH_TOKEN'),
-            'channel' => env('SLACK_BOT_USER_DEFAULT_CHANNEL'),
-        ],
-    ],
-
     'fathom' => [
         'id' => env('FATHOM_SITE_ID'),
     ],
