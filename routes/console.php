1127e11740206c454c030e3a3d465aa5a9906ad3
Jeremiasz Major <jrh.mjr@gmail.com>
2024-05-03 23:22:36 +0200

    prune telscope entries daily

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,9 @@
 
 use App\Console\Commands;
 use Illuminate\Support\Facades\Schedule;
+use Laravel\Telescope\Console\PruneCommand as PruneTelescopeEntries;
 
 Schedule::command(Commands\GenerateSitemap::class)->weekly();
 
+Schedule::command(PruneTelescopeEntries::class)->dailyAt('01:30');
 Schedule::command(Commands\MakeBackup::class)->dailyAt('02:00');
