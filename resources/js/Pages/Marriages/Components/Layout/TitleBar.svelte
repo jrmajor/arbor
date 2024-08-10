882323b434c625436be84167d4e88e29ea47a87d
Jeremiasz Major <jrh.mjr@gmail.com>
2024-08-10 22:25:20 +0200

    migrate marriage title bar to svelte

---
+++ resources/js/Pages/Marriages/Components/Layout/TitleBar.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import type { MarriagePage } from '@/types/resources/marriages';
+	import { t } from '@/helpers/translations';
+
+	let { marriage }: { marriage: MarriagePage } = $props();
+</script>
+
+<span class:invalid={marriage.woman.isDead}>
+	{marriage.woman.name} {marriage.woman.familyName}
+</span>
++
+<span class:invalid={marriage.man.isDead}>
+	{marriage.man.name} {marriage.man.familyName}
+</span>
+
+<small class="text-lg">
+	[{t('marriages.marriage')} №{marriage.id}]
+</small>
