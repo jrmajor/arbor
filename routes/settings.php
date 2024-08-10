eed52fa73e9c0ddf4c8e180f4881989c1fd9dfab
Jeremiasz Major <jrh.mjr@gmail.com>
2024-08-10 21:31:59 +0200

    remove old settings view

--- routes/settings.php
+++ routes/settings.php
@@ -1,11 +1,7 @@
 <?php
 
 use App\Http\Controllers\SettingsController;
-use App\Livewire\Settings;
 use Illuminate\Support\Facades\Route;
-
-Route::get('settings-old', Settings::class)
-    ->middleware('auth')->name('settings');
 
 Route::get('settings', [SettingsController::class, 'edit'])
     ->middleware('auth')->name('settings.edit');
