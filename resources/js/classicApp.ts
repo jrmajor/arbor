9b7e93b8db717ade31d3f798765df9dfa9ef78d2
Jeremiasz Major <jrh.mjr@gmail.com>
2024-08-12 21:43:01 +0200

    remove old person picker

--- resources/js/classicApp.ts
+++ resources/js/classicApp.ts
@@ -1,7 +1,6 @@
 import { Livewire, Alpine } from '../../vendor/livewire/livewire/dist/livewire.esm';
 
 import menuSearch from './classic/menuSearch';
-import './classic/personPicker';
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 (window as any).Alpine = Alpine;
