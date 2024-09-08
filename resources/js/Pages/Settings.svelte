0adc21ca4146b86a05008e0752616f4428abe069
Jeremiasz Major <jrh.mjr@gmail.com>
2024-09-08 16:36:52 +0200

    use input component in settings form

--- resources/js/Pages/Settings.svelte
+++ resources/js/Pages/Settings.svelte
@@ -7,9 +7,7 @@
 
 	let { user }: SharedProps = $props();
 
-	user = user!;
-
-	const emailForm = useForm({ email: user.email });
+	const emailForm = useForm({ email: user!.email });
 	const passwordForm = useForm({ password: '', password_confirmation: '' });
 	const logoutForm = useForm({ password: '' });
 
@@ -34,23 +32,20 @@
 </svelte:head>
 
 <h1 class="mb-3 leading-none text-3xl font-medium">
-	{t('settings.user')}: {user.username}
+	{t('settings.user')}: {user!.username}
 </h1>
 
 <main class="p-6 bg-white rounded-lg shadow">
-	<form onsubmit={submitEmail} class="flex flex-col lg:flex-row">
-		<h2 class="w-full lg:w-1/3 lg:w-1/4 font-medium text-xl text-gray-900 mb-4 sm:mb-0">
+	<form onsubmit={submitEmail} class="flex flex-col">
+		<h2 class="font-medium text-xl text-gray-900 mb-4">
 			{t('settings.email')}
 		</h2>
 
-		<div class="w-full lg:w-2/3 lg:w-3/4">
-			<div class="flex flex-col sm:flex-row space-y-2 sm:space-y-0 sm:space-x-5 items-end sm:items-center">
-				<input
-					type="text"
-					bind:value={$emailForm.email}
-					class="form-input w-full sm:w-auto"
-					class:invalid={$emailForm.errors.email}
-				>
+		<div>
+			<div class="flex flex-col items-end gap-2 sm:flex-row sm:items-center sm:gap-5">
+				<div class="flex w-full sm:w-64">
+					<Form.Input bind:value={$emailForm.email}/>
+				</div>
 				<Button type="submit">{t('misc.save')}</Button>
 			</div>
 			<Form.Error error={$emailForm.errors.email}/>
@@ -59,27 +54,29 @@
 
 	<hr class="mt-7 mb-6 text-gray-200">
 
-	<form onsubmit={submitPassword} class="flex flex-col lg:flex-row">
-		<h2 class="w-full lg:w-1/3 lg:w-1/4 font-medium text-xl text-gray-900 mb-4 sm:mb-0">
+	<form onsubmit={submitPassword} class="flex flex-col">
+		<h2 class="font-medium text-xl text-gray-900 mb-4">
 			{t('settings.change_password')}
 		</h2>
 
-		<div class="w-full lg:w-2/3 lg:w-3/4">
-			<div class="flex flex-col sm:flex-row space-y-2 sm:space-y-0 sm:space-x-5 items-end sm:items-center">
-				<input
-					type="password"
-					bind:value={$passwordForm.password}
-					placeholder={t('settings.password').toLowerCase()}
-					class="form-input w-full sm:w-auto"
-					class:invalid={$passwordForm.errors.password}
-				>
-				<input
-					type="password"
-					bind:value={$passwordForm.password_confirmation}
-					placeholder={t('settings.confirm_password').toLowerCase()}
-					class="form-input w-full sm:w-auto"
-					class:invalid={$passwordForm.errors.password}
-				>
+		<div>
+			<div class="flex flex-col items-end gap-2 sm:flex-row sm:items-center sm:gap-5">
+				<div class="flex w-full sm:w-64">
+					<Form.Input
+						bind:value={$passwordForm.password}
+						type="password"
+						placeholder={t('settings.password').toLowerCase()}
+						error={$passwordForm.errors.password}
+					/>
+				</div>
+				<div class="flex w-full sm:w-64">
+					<Form.Input
+						bind:value={$passwordForm.password_confirmation}
+						type="password"
+						placeholder={t('settings.confirm_password').toLowerCase()}
+						error={$passwordForm.errors.password}
+					/>
+				</div>
 				<Button type="submit">{t('misc.save')}</Button>
 			</div>
 			<Form.Error error={$passwordForm.errors.password}/>
@@ -88,20 +85,21 @@
 
 	<hr class="mt-7 mb-6 text-gray-200">
 
-	<form onsubmit={submitLogout} class="flex flex-col lg:flex-row">
-		<h2 class="w-full lg:w-1/3 lg:w-1/4 font-medium text-xl text-gray-900 mb-4 sm:mb-0">
+	<form onsubmit={submitLogout} class="flex flex-col">
+		<h2 class="font-medium text-xl text-gray-900 mb-4">
 			{t('settings.logout_other_devices')}
 		</h2>
 
-		<div class="w-full lg:w-2/3 lg:w-3/4">
-			<div class="flex flex-col sm:flex-row space-y-2 sm:space-y-0 sm:space-x-5 items-end sm:items-center">
-				<input
-					type="password"
-					bind:value={$logoutForm.password}
-					placeholder={t('settings.password').toLowerCase()}
-					class="form-input w-full sm:w-auto"
-					class:invalid={$logoutForm.errors.password}
-				>
+		<div>
+			<div class="flex flex-col items-end gap-2 sm:flex-row sm:items-center sm:gap-5">
+				<div class="flex w-full sm:w-64">
+					<Form.Input
+						bind:value={$logoutForm.password}
+						type="password"
+						placeholder={t('settings.password').toLowerCase()}
+						error={$logoutForm.errors.password}
+					/>
+				</div>
 				<Button type="submit">{t('settings.logout')}</Button>
 			</div>
 			<Form.Error error={$logoutForm.errors.password}/>
