b18ee3d93aad8fd595c5ea79a00b0056c1bb87be
Jeremiasz Major <jrh.mjr@gmail.com>
2025-01-11 16:59:04 +0100

    fix dropdown icon in person picker

--- resources/js/Components/Forms/PersonPicker.svelte
+++ resources/js/Components/Forms/PersonPicker.svelte
@@ -206,7 +206,7 @@
 
 <style lang="postcss">
 	.dropdown-icon {
-		background-image: url(&quot;data:image/svg+xml;charset=utf-8,%3Csvg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 20 20'%3E%3Cpath stroke='%236b7280' stroke-linecap='round' stroke-linejoin='round' stroke-width='1.5' d='M6 8l4 4 4-4'/%3E%3C/svg%3E&quot;);
+		background-image: url(./dropdown.svg);
 		background-position: right .5rem center;
 		background-repeat: no-repeat;
 		background-size: 1.5em 1.5em;
