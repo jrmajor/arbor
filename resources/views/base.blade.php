ad19b33b6fe94748636ae415e12809143873aad9
Jeremiasz Major <jrh.mjr@gmail.com>
2025-02-01 18:44:49 +0100

    better font size

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -1,5 +1,5 @@
 <!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" style="font-size: .9em">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" style="font-size: .875em">
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
