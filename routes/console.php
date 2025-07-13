95f171e257840d8e0af3b4a69b080f3ad082ef8e
Jeremiasz Major <jrh.mjr@gmail.com>
2025-07-13 20:45:45 +0200

    add grace time to jobs

--- routes/console.php
+++ routes/console.php
@@ -4,5 +4,10 @@ use App\Console\Commands;
 use Illuminate\Support\Facades\Schedule;
 use Laravel\Telescope\Console\PruneCommand as PruneTelescopeEntries;
 
-Schedule::command(PruneTelescopeEntries::class)->dailyAt('01:30');
-Schedule::command(Commands\MakeBackup::class)->dailyAt('02:00');
+Schedule::command(PruneTelescopeEntries::class)
+    ->dailyAt('01:30')
+    ->graceTimeInMinutes(15);
+
+Schedule::command(Commands\MakeBackup::class)
+    ->dailyAt('02:00')
+    ->graceTimeInMinutes(15);
